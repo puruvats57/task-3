2248df4a8a7df4a8412a2776183d3f39b916bb7e
Prateek <prateekvats963@gmail.com>
2024-04-07 23:16:02 +0530

    up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react';
+import axios from 'axios';
 function App() {
+  const [data, setData] = useState([]);
+  const [phonenumber, setphonenumber] = useState();
+  async function send() {
+    try {
+      const response = await axios.post("https://chimpu.xyz/api/post.php", { Post:phonenumber  });
+      setData(prevData => ([...prevData, response.data.data]));
+    } catch (error) {
+      console.log(error);
+    }
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div className="header">
+      {data && data.length > 0 && data.map((item, index) => (
+
+        <p>{item.text}</p>
+      ))
+      }
+      </div >
+      <button onClick={send }></button>
+      </>
+
   );
 }
 
